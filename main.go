53544be3cb6afba39b42b876c1efa72ffc425544
V <none>
2025-06-22 12:44:24 -0500

    added metrics functionality

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+var cfg apiConfig
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("app"))))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("app")))))
 	mux.HandleFunc("/healthz", ready)
+	mux.HandleFunc("/metrics", getMetrics)
+	mux.HandleFunc("/reset", resetMetrics)
 
 	server := http.Server{}
 	server.Addr = ":8080"
@@ -17,8 +27,28 @@ func main() {
 	server.ListenAndServe()
 }
 
+func getMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load()))
+}
+
+func resetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	io.WriteString(w, "Metrics Reset")
+}
+
 func ready(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, "OK")
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(cfg.fileserverHits.Add(1))
+		next.ServeHTTP(w, r)
+	})
 }
