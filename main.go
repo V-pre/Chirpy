5966a6bef97c7a4a1c5a7d4cd25970dae793e0e2
V <none>
2025-06-22 01:02:12 -0500

    added readiness handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net/http"
 )
 
@@ -8,9 +9,16 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", ready)
 
 	server := http.Server{}
 	server.Addr = ":8080"
 	server.Handler = mux
 	server.ListenAndServe()
+}
+
+func ready(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
 }
